3307e11b1f4bb6adf0a36973a4ac14108857607a
kd9041470-beep <kd9041470@gmail.com>
2025-10-24 19:50:14 +0100

    Add BrowserRouter to main.tsx
    Wrap App component in BrowserRouter for routing support.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    {/* basename يضمن أن الروابط تعمل تحت /loval-school-portal/ */}
+    <BrowserRouter basename="/loval-school-portal">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
